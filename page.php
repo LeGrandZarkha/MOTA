be5f48585dc013f3ed37dc6059f528211bcf6795
LeGrandZarkha <cochard.jn@gmail.com>
2024-11-30 11:48:26 +0100

    Added page.php to display simple pages like All Rights Reserved, RGPD and more

---
+++ page.php
@@ -0,0 +1,9 @@
+<?php
+// Appel du header
+get_header();
+?>
+
+
+<?php get_footer(); ?>
+<?php get_template_part('templates/popup') ?>
+<script src="https://kit.fontawesome.com/7992438ddc.js" crossorigin="anonymous"></script>
